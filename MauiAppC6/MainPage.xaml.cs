0081ec902969df1d2d749a1767e48f6fea43ea4c
SANTOSH <SANTOSH@DESKTOP-56MIA02>
2023-12-27 07:26:36 +0545

    Add project files.

---
+++ MauiAppC6/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppC6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiAppC6/Models/TodoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppC6.Models
+{
+    public class TodoModel
+    {
+        public int ID { get; set; }
+        public string? Task { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
